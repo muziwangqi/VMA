4d6bc715886ee73bd909b6f9233e6ac09412f674
qiqili <2819897424@qq.com>
2018-11-16 21:26:45 +0800

    liqiqiupdate

--- app/src/main/java/com/soling/view/activity/BaseActivity.java
+++ app/src/main/java/com/soling/view/activity/BaseActivity.java
@@ -7,23 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.widget.Toast;
 
-<<<<<<< HEAD
+
 import com.soling.R;
 =======
 import com.soling.App;
 import com.soling.R;
 import com.soling.utils.DBHelper;
 import com.soling.utils.MusicFileManager;
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
+
 
 public abstract class BaseActivity extends AppCompatActivity{
 
     private boolean isShowTitle=true;
     private static Toast toast;
-<<<<<<< HEAD
-=======
 
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +30,7 @@ public abstract class BaseActivity extends AppCompatActivity{
         initViews();
         initDatas();
         setListeners();
-<<<<<<< HEAD
+
     }
     public void setTitle(boolean isShow){
         isShowTitle=isShow;
@@ -41,7 +38,7 @@ public abstract class BaseActivity extends AppCompatActivity{
     public void initViews(){};
     public void initDatas(){};
     public void setListeners(){};
-=======
+
 
         // 添加dbHelper hyw
         App app = (App) getApplication();
@@ -57,7 +54,7 @@ public abstract class BaseActivity extends AppCompatActivity{
     public void initViews(){}
     public void initDatas(){}
     public void setListeners(){}
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
+
     public void longToast(String str){
         if (toast==null){
             toast=new Toast(this);
@@ -92,8 +89,4 @@ public abstract class BaseActivity extends AppCompatActivity{
         intent.putExtras(bundle);
         startActivity(intent);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
 }
--- app/src/main/java/com/soling/view/activity/MainActivity.java
+++ app/src/main/java/com/soling/view/activity/MainActivity.java
@@ -48,8 +48,7 @@ public class MainActivity extends BaseActivity implements OnClickListener {
 				.inflate(R.layout.fragment_phone, null);
 		View layoutMusic = layoutInflater
 				.inflate(R.layout.fragment_music, null);*/
-<<<<<<< HEAD
-//<<<<<<< HEAD
+
 		viewPager = (ViewPager) findViewById(R.id.id_vp_scoll);
 		tvPhone = (TextView) findViewById(R.id.id_tv_phone);
 		tvMusic = (TextView) findViewById(R.id.id_tv_music);
@@ -63,8 +62,7 @@ public class MainActivity extends BaseActivity implements OnClickListener {
 		
 		viewPager.setAdapter(new ScollAdapter(getSupportFragmentManager(),fragments));
 		//System.out.println("setAdapter");
-//=======
-=======
+
 //		viewPager = (ViewPager) findViewById(R.id.id_vp_scoll);
 //		tvPhone = (TextView) findViewById(R.id.id_tv_phone);
 //		tvMusic = (TextView) findViewById(R.id.id_tv_music);
@@ -78,7 +76,7 @@ public class MainActivity extends BaseActivity implements OnClickListener {
 //
 //		viewPager.setAdapter(new ScollAdapter(getSupportFragmentManager(),fragments));
 //		//System.out.println("setAdapter");
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
+
         viewPager = (ViewPager) findViewById(R.id.id_vp_scoll);
         tvPhone = (TextView) findViewById(R.id.id_tv_phone);
         tvMusic = (TextView) findViewById(R.id.id_tv_music);
@@ -114,7 +112,7 @@ public class MainActivity extends BaseActivity implements OnClickListener {
             }
         });
     }
-//>>>>>>> 6d0060cba469c5e8dc4bc00558bab81f6e8d0212
+
 
     public void onClick(View view) {
         switch (view.getId()) {
