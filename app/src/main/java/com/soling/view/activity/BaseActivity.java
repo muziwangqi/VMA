9b8657dc37d09cc1bc20eb989369940343db1a42
unknown <1605885413@qq.com>
2018-11-19 09:41:54 +0800

    处理乱码

--- app/src/main/java/com/soling/view/activity/BaseActivity.java
+++ app/src/main/java/com/soling/view/activity/BaseActivity.java
@@ -3,27 +3,17 @@ package com.soling.view.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-import com.soling.R;
-=======
 import com.soling.App;
-import com.soling.R;
 import com.soling.utils.DBHelper;
-import com.soling.utils.MusicFileManager;
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
 
 public abstract class BaseActivity extends AppCompatActivity{
 
     private boolean isShowTitle=true;
     private static Toast toast;
-<<<<<<< HEAD
-=======
 
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,22 +23,14 @@ public abstract class BaseActivity extends AppCompatActivity{
         initViews();
         initDatas();
         setListeners();
-<<<<<<< HEAD
+
     }
-    public void setTitle(boolean isShow){
-        isShowTitle=isShow;
-    }
-    public void initViews(){};
-    public void initDatas(){};
-    public void setListeners(){};
-=======
 
         // 添加dbHelper hyw
         App app = (App) getApplication();
         if (app.getDbHelper() == null) {
             app.setDbHelper(new DBHelper(this, DBHelper.DATABASE_NAME, null, DBHelper.VERSION));
         }
-    }
 
     public void setTitle(boolean isShow){
         isShowTitle=isShow;
@@ -57,7 +39,7 @@ public abstract class BaseActivity extends AppCompatActivity{
     public void initViews(){}
     public void initDatas(){}
     public void setListeners(){}
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
+
     public void longToast(String str){
         if (toast==null){
             toast=new Toast(this);
@@ -92,8 +74,4 @@ public abstract class BaseActivity extends AppCompatActivity{
         intent.putExtras(bundle);
         startActivity(intent);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> f01468819ae371d307634f267ddf256a2db0e29c
 }
